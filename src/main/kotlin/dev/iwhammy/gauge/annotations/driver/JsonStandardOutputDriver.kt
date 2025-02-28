493e3a5bdd4bcb730ea84d4519ecc350dfe21286
i-whammy <yas.iwami@gmail.com>
2025-02-28 22:04:59 +0900

    kotlinx serialization

--- src/main/kotlin/dev/iwhammy/gauge/annotations/driver/JsonStandardOutputDriver.kt
+++ src/main/kotlin/dev/iwhammy/gauge/annotations/driver/JsonStandardOutputDriver.kt
@@ -2,24 +2,18 @@ package dev.iwhammy.gauge.annotations.driver
 
 import dev.iwhammy.gauge.annotations.domain.Report
 import dev.iwhammy.gauge.annotations.usecase.port.OutputPort
-import jakarta.json.Json
-import jakarta.json.JsonObject
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
 class JsonStandardOutputDriver() : OutputPort {
     override fun output(gaugeUsageReport: Report.GaugeUsageReport) {
         gaugeUsageReport.usage.steps().also { println(GaugeUsageOutput(it).toJson()) }
     }
 
+    @Serializable
     data class GaugeUsageOutput(val steps: List<String>)
 }
 
-fun JsonStandardOutputDriver.GaugeUsageOutput.toJson(): JsonObject {
-    val arrayBuilder = Json.createArrayBuilder()
-    steps.forEach {
-        arrayBuilder.add(it)
-    }
-
-    return Json.createObjectBuilder()
-        .add("steps", arrayBuilder.build())
-        .build()
+fun JsonStandardOutputDriver.GaugeUsageOutput.toJson(): String {
+    return Json.encodeToString(this)
 }
